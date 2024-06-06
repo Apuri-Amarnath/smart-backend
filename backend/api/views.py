f43a2e3a85f16394fc739de7bd39a6df5a21fe24
Apuri-Amarnath <apuriamarnath123@gmail.com>
2024-06-06 17:15:01 +0530

    semester-registration

--- backend/api/views.py
+++ backend/api/views.py
@@ -369,11 +369,10 @@ class SemesterRegistrationViewset(viewsets.ModelViewSet):
 
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
-        headers = self.get_success_headers(serializer.data)
-        return Response(serializer.data, {'message': 'Semester registration successful'},
-                        status=status.HTTP_201_CREATED, headers=headers)
+        if serializer.is_valid(raise_exception=True):
+            self.perform_create(serializer)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def perform_create(self, serializer):
         serializer.save()
