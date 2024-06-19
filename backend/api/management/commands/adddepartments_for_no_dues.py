19ce5bc54755ce32de61f11106929b5a3bfbe1d8
Apuri-Amarnath <apuriamarnath123@gmail.com>
2024-06-19 21:45:12 +0530

    added cgpa field

---
+++ backend/api/management/commands/adddepartments_for_no_dues.py
@@ -0,0 +1,110 @@
+from django.core.management.base import BaseCommand
+from api.models import Departments_for_no_due
+
+
+class Command(BaseCommand):
+    help = 'Adds all departments for No due requests '
+
+    def handle(self, *args, **options):
+        default_departments = [
+            {
+                'Department_name': 'Electrical & Electronics Laboratory',
+                'status': 'waiting for approval',  # Default status
+                'approved': False,  # Default approved status
+            },
+            {
+                'Department_name': 'Physics Laboratory',
+                'status': 'waiting for approval',
+                'approved': False,
+            },
+            {
+                'Department_name': 'Chemistry Laboratory',
+                'status': 'waiting for approval',
+                'approved': False,
+            },
+            {
+                'Department_name': 'Mechanics Laboratory',
+                'status': 'waiting for approval',
+                'approved': False,
+            },
+            {
+                'Department_name': 'Civil Lab',
+                'status': 'waiting for approval',
+                'approved': False,
+            },
+            {
+                'Department_name': 'Workshop',
+                'status': 'waiting for approval',
+                'approved': False,
+            },
+            {
+                'Department_name': 'Computer Laboratory',
+                'status': 'waiting for approval',
+                'approved': False,
+            },
+            {
+                'Department_name': 'KYP Lab/BSDM',
+                'status': 'waiting for approval',
+                'approved': False,
+            },
+            {
+                'Department_name': 'TEQIP',
+                'status': 'waiting for approval',
+                'approved': False,
+            },
+            {
+                'Department_name': 'Examination Section',
+                'status': 'waiting for approval',
+                'approved': False,
+            },
+            {
+                'Department_name': 'Sports Section',
+                'status': 'waiting for approval',
+                'approved': False,
+            },
+            {
+                'Department_name': 'General Store',
+                'status': 'waiting for approval',
+                'approved': False,
+            },
+            {
+                'Department_name': 'Hostel',
+                'status': 'waiting for approval',
+                'approved': False,
+            },
+            {
+                'Department_name': 'Library',
+                'status': 'waiting for approval',
+                'approved': False,
+            },
+            {
+                'Department_name': 'Academic Section',
+                'status': 'waiting for approval',
+                'approved': False,
+            },
+            {
+                'Department_name': 'Cash Section',
+                'status': 'waiting for approval',
+                'approved': False,
+            },
+            {
+                'Department_name': 'Accounts Section',
+                'status': 'waiting for approval',
+                'approved': False,
+            },
+            {
+                'Department_name': 'HoD, EEE / ME / CSE / CE / CSE-AI / CE with CA',
+                'status': 'waiting for approval',
+                'approved': False,
+            },
+        ]
+
+        for department_data in default_departments:
+            Departments_for_no_due.objects.get_or_create(
+                Department_name=department_data['Department_name'],
+                defaults={
+                    'status': department_data['']
+
+                }
+        )
+        self.stdout.write(self.style.SUCCESS('departments populated successfully'))
