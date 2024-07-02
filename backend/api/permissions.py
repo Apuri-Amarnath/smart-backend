2c1a05846e1bc9eaab29f01c80f70a87f51d94a3
Apuri-Amarnath <apuriamarnath123@gmail.com>
2024-07-03 01:24:29 +0530

    mess fee update

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -12,10 +12,10 @@ class IsStudentOrAdmin(permissions.BasePermission):
 
 
 class IsTeacherOrAdmin(permissions.BasePermission):
-    def has_object_permission(self, request, view):
+    def has_permission(self, request, view):
         return request.user and (request.user.role == 'teacher' or request.user.role == 'admin')
 
 
 class IsFacultyOrAdmin(permissions.BasePermission):
-    def has_object_permission(self, request, view):
+    def has_permission(self, request, view):
         return request.user and (request.user.role == 'faculty' or request.user.role == 'admin')
